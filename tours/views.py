24b99fd98f28f10ad116f4e3f5a37fac8508a65e
Vyacheslav Bataev <98668962+unlessiamwrong@users.noreply.github.com>
2022-07-25 00:06:25 +0500

    Update views.py

--- tours/views.py
+++ tours/views.py
@@ -15,7 +15,7 @@ def tour_view(request: WSGIRequest, id: int) -> HttpResponse:
 
 
 def custom_handler404(request: WSGIRequest, exception) -> HttpResponse:
-    return render(request: WSGIRequest, '404.html', status=400)
+    return render(request, '404.html', status=400)
 
 
 def custom_handler500(request: WSGIRequest) -> HttpResponse:
