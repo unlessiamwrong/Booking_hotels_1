77a14b3657ae511e59af17ac912e915128da086c
Vyacheslav Bataev <98668962+unlessiamwrong@users.noreply.github.com>
2022-07-24 23:44:00 +0500

    Update views.py

--- tours/views.py
+++ tours/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
+from django.core.handlers.wsgi import WSGIRequest
+from django.http.response import HttpResponse
 
-
-def main_view(request):
+def main_view(request:WSGIRequest) -> HttpResponse:
     return render(request, 'index.html')
 
 
-def departure_view(request, departure):
+def departure_view(request:WSGIRequest, departure) -> HttpResponse:
     return render(request, 'departure.html')
 
 
-def tour_view(request, id):
+def tour_view(request:WSGIRequest, id) -> HttpResponse:
     return render(request, 'tour.html')
 
 
-def custom_handler404(request, exception):
-    return render(request, '404.html', status=400)
+def custom_handler404(request:WSGIRequest, exception) -> HttpResponse:
+    return render(request:WSGIRequest, '404.html', status=400)
 
 
-def custom_handler500(request):
+def custom_handler500(request:WSGIRequest) -> HttpResponse:
     return render(request, '500.html', status=500)
